7e671c319db813503437b75bc8ec049cf6e89864
lmx2315 <lmx2315@mail.ru>
2020-04-04 14:26:34 +0300

    zzz

--- TB_MASTER_START/writer_com_mem.sv
+++ TB_MASTER_START/writer_com_mem.sv
@@ -30,6 +30,7 @@ logic [337:0]    data_sig           =0;
 logic [337:0] 	 w_REG_DATA         =0;//данные для записи в реестр
 logic [  7:0] 	 w_REG_ADDR         =0;//адрес в реестре куда можно делать свежую запись
 logic [  7:0]	 tmp_REG_ADDR		=0;//
+logic [  7:0]    clr_REG_ADDR 		=0;//адресс под очистку
 logic 		   	 WR_REG	            =0;//сигнал записи в память реестра
 logic 			 FLAG_WORK_PROCESS	=0;//сигнал что идёт какой-то процесс
 
@@ -66,7 +67,7 @@ assign data_sig = {tmp_TIME_START,tmp_FREQ        ,tmp_FREQ_STEP  ,tmp_FREQ_RATE
 				   tmp_N_impulse ,tmp_TYPE_impulse,tmp_Interval_Ti,tmp_Interval_Tp,tmp_Tblank1,tmp_Tblank2};
 //------------------------блок записи данных в память----------------------
 enum {idle,start,clrear,cycle,end_cycle} clr_state,clr_next_state;
-enum {clr_all,clr_data,wr_data,idle    } status; 
+enum {clr_all,clr_data,wr_data,idle    } status,next_status; 
 
 always_comb
  begin
@@ -77,28 +78,48 @@ always_comb
 	endcase
 end
 
+always_comb
+ begin
+	case (status)
+		 clr_all:next_status=idle;
+		clr_data:next_status=idle;
+		 wr_data:next_status=idle;
+	endcase
+end
 
 
-always_ff @(posedge CLK) begin 
-	if(~rst_n) begin
+always_ff @(posedge CLK) 
+begin 
+	if(~rst_n) 
+	begin
 	FLAG_WORK_PROCESS <= 0;
 	end else 
-	if (status==clr_all) 
+	if (status==clr_all) //режим очистки памяти
 	begin
 		 if (tmp_REG_ADDR<N_IDX) 
 		 	begin
 		 		tmp_REG_ADDR<=tmp_REG_ADDR+1'b1;
-		 		WR_REG     <=1'b1;
-		 		w_REG_DATA <={64'hffffffff_ffffffff,273'h0000};
-		 	end
-		 else 
-		 	begin
-		 		status<=idle;
-		 		WR_REG     <=1'b1;
-		 	end		 
+		 		WR_REG      <=1'b1;
+		 		w_REG_DATA  <={64'hffffffff_ffffffff,273'h0000};
+		 	end	 
+	end else
+	if (status==clr_data) //режим удаления команды
+	begin
+		tmp_REG_ADDR<=clr_REG_ADDR;//записываем адресс удаляемой строки из памяти
+		WR_REG      <=1'b1;
+	end else
+	if (status==idle)
+	begin
+		WR_REG      <=1'b0;
+	end else
+	if (status==wr_data)
+	begin
+		tmp_REG_ADDR<=w_REG_ADDR;
+		WR_REG      <=1'b1;
+		w_REG_DATA  <=data_sig;
 	end
 end
-
+//------блок чтения памяти и поиска вакантной строчки -----------
 always_ff @(posedge CLK)
 begin
 	w_REG_DATA<=data_sig;
