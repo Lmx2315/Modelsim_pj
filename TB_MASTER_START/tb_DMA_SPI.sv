f481deac60d046ec35dbb144e7cba353d60b9485
Lmx2315 <lmx2315@mail.ru>
2020-04-18 19:17:41 +0300

    zzz

--- TB_MASTER_START/tb_DMA_SPI.sv
+++ TB_MASTER_START/tb_DMA_SPI.sv
@@ -46,8 +46,51 @@ always #20         SCLK=~SCLK;
 	logic        SPI_WR;
 	logic [407:0] data_reg=0;
 
+	logic [407:0] test_data;
+	logic [  7:0]  DATA_out;
+	logic 		   TxD_busy;	 
+	logic 		   SEND;
 
-	DMA_SPI inst_DMA_SPI
+	logic [ 63:0]	 tmp_TIME 		  ;
+	logic [ 47:0] 	 tmp_FREQ 		  ;
+	logic [ 47:0] 	 tmp_FREQ_STEP 	  ;
+	logic [ 31:0] 	 tmp_FREQ_RATE	  ;
+	logic [ 63:0]    tmp_TIME_START   ;
+	logic [ 15:0]    tmp_N_impulse    ;
+	logic [  7:0]    tmp_TYPE_impulse ;
+	logic [ 31:0]    tmp_Interval_Ti  ;
+	logic [ 31:0]    tmp_Interval_Tp  ;
+	logic [ 31:0]    tmp_Tblank1	  ;
+	logic [ 31:0]    tmp_Tblank2	  ;
+
+	assign test_data={tmp_TIME       ,tmp_FREQ     ,tmp_FREQ_STEP   ,tmp_FREQ_RATE  ,
+					  tmp_TIME_START ,tmp_N_impulse,tmp_TYPE_impulse,tmp_Interval_Ti,
+					  tmp_Interval_Tp,tmp_Tblank1  ,tmp_Tblank2};
+//---------------------------------------------
+send_to_uart u1
+		(
+			.clk      (clk_48),
+			.rst_n    (rst_n),
+			.data_in  (test_data),
+			.RCV      (SPI_WR),
+			.BUSY     (TxD_busy),
+			.DATA_out (DATA_out),
+			.SEND     (SEND)
+		);
+
+
+async_transmitter #(
+			.ClkFrequency(48000000),
+			.Baud		 (9600    )
+		) tx1 (
+			.clk       (clk_48),
+			.TxD_start (SEND),
+			.TxD_data  (DATA_out),
+			.TxD       (),
+			.TxD_busy  (TxD_busy)
+		);
+//----------------------------------------------
+	DMA_SPI spi1
 		(
 			.clk             (clk_48),
 			.clk_en          (clk_en),
@@ -55,9 +98,10 @@ always #20         SCLK=~SCLK;
 			.MOSI            (MOSI),
 			.CS              (CS),
 			.SCLK            (SCLK),
-			.TIME            (),
+//-----------------------------------------------
+			.TIME            (tmp_TIME),
 			.SYS_TIME_UPDATE (),
-			.FREQ            (),
+			.FREQ            (tmp_FREQ),
 			.FREQ_STEP       (),
 			.FREQ_RATE       (),
 			.TIME_START      (),
@@ -67,6 +111,7 @@ always #20         SCLK=~SCLK;
 			.Interval_Tp     (),
 			.Tblank1         (),
 			.Tblank2         (),
+//----------------------------------------------
 			.SPI_WR          (SPI_WR)
 		);
 
